openGDA/gda-epics

commit 6eb29ce5fc9cfea3b2cef6f33f858276a097eb69

JEEP-108 - Check for live mode before arming camera, to enable test on epics simulation.

uk.ac.gda.epics/src/gda/device/detector/addetector/triggering/SingleExposurePco.java
@@ -21,6 +21,7 @@ package gda.device.detector.addetector.triggering;
 import gda.device.detector.areadetector.v17.ADBase;
 import gda.device.detector.areadetector.v17.ADDriverPco;
 import gda.device.detector.areadetector.v17.ADDriverPco.PcoTriggerMode;
+import gda.util.LiveModeUtil;
 
 public class SingleExposurePco extends SingleExposureStandard {
 
@@ -34,22 +35,24 @@ public class SingleExposurePco extends SingleExposureStandard {
 	@Override
 	public void prepareForCollection(double collectionTime, int numImages) throws Exception {
 		super.prepareForCollection(collectionTime, numImages);
-		getAdBase().setAcquirePeriod(0.); //for pco always set acq period to 0 to force delay to 0.
-		adDriverPco.getArmModePV().putCallback(true);
+		getAdBase().setAcquirePeriod(0.); // for pco always set acq period to 0 to force delay to 0.
+		if (LiveModeUtil.isLiveMode()) {
+			adDriverPco.getArmModePV().putCallback(true);
+		}
 	}
 
 	@Override
 	public void completeCollection() throws Exception {
 		super.completeCollection();
-		adDriverPco.getArmModePV().putCallback(false);
+		if (LiveModeUtil.isLiveMode()) {
+			adDriverPco.getArmModePV().putCallback(false);
+		}
 	}
-	
-	
 
 	@Override
 	protected void configureTriggerMode() throws Exception {
 		// Reported Epics bug: changing mode while acquiring causes an IOC crash (28oct2011 RobW)
-		getAdBase().stopAcquiring(); 
+		getAdBase().stopAcquiring();
 		getAdBase().setTriggerMode(PcoTriggerMode.SOFTWARE.ordinal());
 	}
 

commit 07a0e4ea138060c2808298b713ad7905b665376e

MTGDA-104, GDA-5379 - Fix ADPcoTest failure introduced by 6eb29ce
Updated test to reflect okay change to SingleExposurePco.

uk.ac.gda.epics.test/src/gda/device/detector/addetector/ADPcoTest.java
@@ -24,6 +24,7 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import gda.TestHelpers;
+import gda.configuration.properties.LocalProperties;
 import gda.device.detector.addetector.ADDetector;
 import gda.device.detector.addetector.ADPco;
 import gda.device.detector.addetector.triggering.SingleExposurePco;
@@ -33,6 +34,8 @@ import gda.device.detector.areadetector.v17.NDFile.FileWriteMode;
 import gda.device.detector.areadetector.v17.NDPluginBase;
 import gda.epics.PV;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InOrder;
 
@@ -44,7 +47,18 @@ public class ADPcoTest extends ADDetectorTest {
 	private PV<Boolean> mockArmModePv;
 	private NDPluginBase mockNdFilePluginBase;
 
-
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		LocalProperties.set("gda.mode", "live");
+	}
+	
+	@After
+	public void tearDown() {
+		LocalProperties.clearProperty("gda.mode");
+	}
+	
 	@Override
 	public ADDetector det() {
 		return adPco;