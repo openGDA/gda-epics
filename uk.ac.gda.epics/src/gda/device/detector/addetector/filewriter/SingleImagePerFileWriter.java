openGDA/gda-epics

commit 1975d115983d42d0f9321b0ee67017b802fa600e

BLXVI-48: SingleImagePerFileWriter now properly populates metadata with path template

uk.ac.gda.epics/src/gda/device/detector/addetector/filewriter/SingleImagePerFileWriter.java
@@ -182,13 +182,16 @@ public class SingleImagePerFileWriter extends FileWriterBase {
 		if (existingMetadataString==null) {
 			newMetadataString = "";
 		} else {
-			newMetadataString = existingMetadataString + "\n";
+			newMetadataString = existingMetadataString;
 		}
 		
-		String newValue = StringUtils.replaceOnce(getFileTemplate(), "%s", getFilePathRelativeToDataDirIfPossible() + "/");
+		String filePathRelativeToDataDirIfPossible = getFilePathRelativeToDataDirIfPossible();
+		String fileRedoutTemplate = getFileTemplateForReadout();
+		String newValue = StringUtils.replaceOnce(fileRedoutTemplate, "%s", filePathRelativeToDataDirIfPossible + "/");
 		newValue = StringUtils.replaceOnce(newValue, "%s", getFileName());
 		String newKey = getkeyNameForMetadataPathTemplate();
-		jythonNamespace.placeInJythonNamespace("SRSWriteAtFileCreation", newMetadataString + newKey + "='" +newValue + "'");
+		jythonNamespace.placeInJythonNamespace("SRSWriteAtFileCreation", newMetadataString + newKey + "='" +newValue + "'\n");
+		InterfaceProvider.getTerminalPrinter().print("Image location: " + newKey + "='" +newValue);
 	}
 
 	protected void configureNdFile() throws Exception {

commit 507d50c09397ba700cce3d08bc56064b4d3312af

BLXVI-48:  Fixed regression introduced in 1975d115 - SingleImagePerFileWriter now properly populates metadata with path template,

uk.ac.gda.epics/src/gda/device/detector/addetector/filewriter/SingleImagePerFileWriter.java
@@ -199,8 +199,8 @@ public class SingleImagePerFileWriter extends FileWriterBase {
 		}
 		
 		String filePathRelativeToDataDirIfPossible = getFilePathRelativeToDataDirIfPossible();
-		String fileRedoutTemplate = getFileTemplateForReadout();
-		String newValue = StringUtils.replaceOnce(fileRedoutTemplate, "%s", filePathRelativeToDataDirIfPossible + "/");
+		String template = (getFileTemplateForReadout() == null) ? getFileTemplate() : getFileTemplateForReadout();
+		String newValue = StringUtils.replaceOnce(template, "%s", filePathRelativeToDataDirIfPossible + "/");
 		newValue = StringUtils.replaceOnce(newValue, "%s", getFileName());
 		String newKey = getkeyNameForMetadataPathTemplate();
 		jythonNamespace.placeInJythonNamespace("SRSWriteAtFileCreation", newMetadataString + newKey + "='" +newValue + "'\n");