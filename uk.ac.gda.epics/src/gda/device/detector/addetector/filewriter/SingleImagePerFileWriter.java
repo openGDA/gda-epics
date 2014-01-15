openGDA/gda-epics

commit 38ed0a02bc52340147726ef9f44febe12275349d

MT-GDA (8.36) Add option to suppress "Path does not exist on IOC" errors.
Several of the Area Detectors on MT beamlines incorrectly report that a
path does not exist when it does so in order to use these area detectors
these errors must be suppressed.

uk.ac.gda.epics/src/gda/device/detector/addetector/filewriter/SingleImagePerFileWriter.java
@@ -171,8 +171,11 @@ public class SingleImagePerFileWriter extends FileWriterBase implements NXPlugin
 		if (isSetFileNameAndNumber()) {
 			configureNdFile();
 		} else {
-			if (!getNdFile().filePathExists())
-				throw new Exception("Path does not exist on IOC");
+			if( !getNdFile().filePathExists())
+				if (isPathErrorSuppressed())
+					logger.warn("Ignoring Path does not exist on IOC '" + filePath + "'");
+				else
+					throw new Exception("Path does not exist on IOC '" + filePath + "'");
 		}
 		clearWriteStatusErr();
 

uk.ac.gda.epics/src/gda/device/detector/addetector/filewriter/FileWriterBase.java
@@ -49,6 +49,8 @@ public abstract class FileWriterBase implements NXFileWriterPlugin, Initializing
 
 	private boolean setFileNameAndNumber = true;
 
+	private boolean pathErrorSuppressed=false;
+
 	private Observable<Short> writeStatusObservable;
 
 	private boolean writeStatusErr=false;
@@ -215,7 +217,14 @@ public abstract class FileWriterBase implements NXFileWriterPlugin, Initializing
 		return setFileNameAndNumber;
 	}
 
-	
+	public boolean isPathErrorSuppressed() {
+		return pathErrorSuppressed;
+	}
+
+	public void setPathErrorSuppressed(boolean pathErrorSuppressed) {
+		this.pathErrorSuppressed = pathErrorSuppressed;
+	}
+
 	/**
 	 * @return the file path to configure in AreaDetector
 	 */

uk.ac.gda.epics/src/gda/device/detector/addetector/filewriter/MultipleImagesPerHDF5FileWriter.java
@@ -271,9 +271,11 @@ public class MultipleImagesPerHDF5FileWriter extends FileWriterBase implements N
 		String filePath = getFilePath();
 		getNdFile().setFilePath(filePath);
 		if( !getNdFile().filePathExists())
-			throw new Exception("Path does not exist on IOC '" + filePath + "'");		
+			if (isPathErrorSuppressed())
+				logger.warn("Ignoring Path does not exist on IOC '" + filePath + "'");
+			else
+				throw new Exception("Path does not exist on IOC '" + filePath + "'");
 		long scanNumber = getScanNumber();
-
 		
 		getNdFile().setFileNumber((int)scanNumber);	
 		getNdFile().setAutoSave((short) 0);

uk.ac.gda.epics/src/gda/device/detector/addetector/filewriter/MultipleImagesPerParallelHDF5FileWriter.java
@@ -333,9 +333,11 @@ public class MultipleImagesPerParallelHDF5FileWriter extends FileWriterBase impl
 			filePath += File.separator;
 		getNdFile().setFilePath(filePath);
 		if( !getNdFile().filePathExists())
-			throw new Exception("Path does not exist on IOC '" + filePath + "'");		
+			if (isPathErrorSuppressed())
+				logger.warn("Ignoring Path does not exist on IOC '" + filePath + "'");
+			else
+				throw new Exception("Path does not exist on IOC '" + filePath + "'");
 		long scanNumber = getScanNumber();
-
 		
 		getNdFile().setFileNumber((int)scanNumber);	
 		getNdFile().setAutoSave((short) 0);

commit d81f81111e6ca1e1438bf4380233876ff0c21c84

MT-GDA (8.36) Fix error introduced in 38ed0a0.

uk.ac.gda.epics/src/gda/device/detector/addetector/filewriter/SingleImagePerFileWriter.java
@@ -173,9 +173,9 @@ public class SingleImagePerFileWriter extends FileWriterBase implements NXPlugin
 		} else {
 			if( !getNdFile().filePathExists())
 				if (isPathErrorSuppressed())
-					logger.warn("Ignoring Path does not exist on IOC '" + filePath + "'");
+					logger.warn("Ignoring Path does not exist on IOC");
 				else
-					throw new Exception("Path does not exist on IOC '" + filePath + "'");
+					throw new Exception("Path does not exist on IOC");
 		}
 		clearWriteStatusErr();
 